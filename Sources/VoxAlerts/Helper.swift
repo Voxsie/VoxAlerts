b8df2d0ae5ac3b794cfe8202a381edf15e4c3ccc
Ilya <izheltikov0@gmail.com>
2023-05-08 17:23:04 +0300

    ups height

--- Sources/VoxAlerts/Helper.swift
+++ Sources/VoxAlerts/Helper.swift
@@ -23,7 +23,7 @@ final class AlertHelper {
         NSLayoutConstraint.activate([
             alertView.leadingAnchor.constraint(equalTo: viewController.view.leadingAnchor, constant: 16),
             alertView.trailingAnchor.constraint(equalTo: viewController.view.trailingAnchor, constant: -16),
-            
+            alertView.heightAnchor.constraint(greaterThanOrEqualToConstant: 100)
         ])
         
         switch(position) {
